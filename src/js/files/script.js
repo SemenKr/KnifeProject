133e69e5701c019ce09eea12dfc559ef210bd49b
Семён Крекотун <samkrekotyn@gmail.com>
2022-11-30 00:02:40 +0300

    работа над подменю

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,4 +1,30 @@
 // Подключение функционала "Чертогов Фрилансера"
-import { isMobile } from "./functions.js";
+// import { cli } from "webpack-dev-server";
+// import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
-import { flsModules } from "./modules.js";
+// import { flsModules } from "./modules.js";
+
+//========================================================================================================================================================
+
+// snipped click
+document.addEventListener("click", documentActions);
+
+function documentActions(e) {
+  const targetElement = e.target;
+  if (targetElement.closest("[data-parent]")) {
+    const subMenuId = targetElement.dataset.parent
+      ? targetElement.dataset.parent
+      : null;
+
+    const subMenu = document.querySelector(`[data-submenu="${subMenuId}"]`);
+    const catalogMenu = document.querySelector(".catalog-header");
+
+    if (subMenu) {
+      targetElement.classList.toggle("_sub-menu-active");
+      subMenu.classList.toggle("_sub-menu-open");
+    } else {
+      console.log("Oh Something wrong(");
+    }
+    e.preventDefault();
+  }
+}
